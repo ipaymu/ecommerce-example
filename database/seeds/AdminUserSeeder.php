9b1b20f5823e1766d172ae92f7dbd49f1c5e4db6
Krisna Anggara <krisna@ipaymu.com>
2019-10-15 11:16:44 +0800

    update Seeder

--- database/seeds/AdminUserSeeder.php
+++ database/seeds/AdminUserSeeder.php
@@ -13,9 +13,9 @@ class AdminUserSeeder extends Seeder
     public function run()
     {
         AdminUser::create([
-           'name' => 'jhon',
-            'email' => 'jhon@example.com',
-            'password' => '$2y$10$Gkoho102gT2niXU2EQhUNuMJ7LYC81WSS/TZzs0T6/VxoXxAiS7le',
+           'name' => 'Admin',
+            'email' => 'admin@admin.com',
+            'password' => bcrypt("password"),
         ]);
     }
 }
